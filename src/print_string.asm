9f81bedd882d6d2e99fa72d1f3da79e363fe234d
Areshisu <Alexis.Emmanuel.Mtz@Gmail.com>
2026-03-14 15:13:36 -0600

    Enhance comments in print_string.asm
    Added comments to clarify the purpose of the code.

--- src/print_string.asm
+++ src/print_string.asm
@@ -14,9 +14,11 @@ print_string:
     push ebp
     mov ebp, esp
 
+    ; Guardamos la dirección inicial de la cadena y encontramos su longitud.
     mov ecx, eax
     xor edx, edx
 
+; Revisa si llegamos al final, si no avanza al siguiente caracter y aumenta la longitud.
 contar:
     cmp byte [ecx], 0
     je imprimir
