a60f25567f3bd2d9430773467badbda78a93b8f4
Areshisu <Alexis.Emmanuel.Mtz@Gmail.com>
2026-03-14 14:56:31 -0600

    Refactor newline function to remove redundant code
    Removed redundant line for setting ebp in newline function.

--- src/newline.asm
+++ src/newline.asm
@@ -5,12 +5,17 @@ SECTION .text
 
 newline:
 
+; Prólogo
     push ebp
-    mov ebp, esp
+    mov ebp, esp 
 
+; Imprime un salto de linea.
     mov al, 10
+
+; Llama la función que imprime el caracter.
     call print_char
 
+; Epílogo
     mov esp, ebp
     pop ebp
     ret
