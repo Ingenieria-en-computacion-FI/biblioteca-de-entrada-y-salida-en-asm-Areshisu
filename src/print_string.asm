03d43211f9929ac3cb5d879f3ff7fe9e1cdb0e16
Areshisu <Alexis.Emmanuel.Mtz@Gmail.com>
2026-03-14 15:10:49 -0600

    Add string length calculation and syscall for printing
    Implemented string length calculation and write syscall.

--- src/print_string.asm
+++ src/print_string.asm
@@ -10,13 +10,27 @@ global print_string
 
 print_string:
 
+    ; Prólogo
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. calcular longitud
-    ; 2. syscall write
+    mov ecx, eax
+    xor edx, edx
 
+contar:
+    cmp byte [ecx], 0
+    je imprimir
+    inc ecx
+    inc edx
+    jmp  contar
+
+imprimir
+    mov ecx, ax,
+    mov eax, 4
+    moc ebx, 1
+    int 0x80
+
+    ; Epílogo
     mov esp, ebp
     pop ebp
     ret
